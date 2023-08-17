0963f02cb92c22602d0303e634acd60e9aaeae23
Muhammad Abuar <muhammadabuzar010@gmail.com>
2023-08-17 16:28:36 +0500

    CRUD

--- graphql/schema.js
+++ graphql/schema.js
@@ -44,6 +44,7 @@ const UserOrderType = new GraphQLObjectType({
   },
 });
 
+// Resolvers
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
   fields: {
@@ -53,7 +54,6 @@ const RootQuery = new GraphQLObjectType({
         email: { type: GraphQLString },
       },
       async resolve(parent, args) {
-        // Import the User model
         const User = require("../models/users");
 
         try {
@@ -99,6 +99,84 @@ const RootQuery = new GraphQLObjectType({
   },
 });
 
+// Mutation
+
+const CreationResponseType = new GraphQLObjectType({
+  name: "CreationResponse",
+  fields: {
+    message: { type: GraphQLString },
+    email: { type: GraphQLString },
+  },
+});
+
+const DeletionResponseType = new GraphQLObjectType({
+  name: "DeletionResponse",
+  fields: {
+    email: { type: GraphQLString },
+    message: { type: GraphQLString },
+  },
+});
+
+const Mutation = new GraphQLObjectType({
+  name: "Mutation",
+  fields: {
+    createUser: {
+      type: CreationResponseType,
+      args: {
+        email: { type: GraphQLString },
+        username: { type: GraphQLString },
+        password: { type: GraphQLString },
+      },
+      async resolve(parent, args) {
+        const User = require("../models/users");
+
+        try {
+          const newUser = new User({
+            email: args.email,
+            username: args.username,
+            password: args.password,
+          });
+
+          const createdUser = await newUser.save();
+          return {
+            message: `User created with email: ${createdUser.email}`,
+            email: createdUser.email,
+          };
+        } catch (error) {
+          throw new Error("Error creating user: " + error.message);
+        }
+      },
+    },
+    deleteUser: {
+      type: DeletionResponseType,
+      args: {
+        email: { type: GraphQLString },
+      },
+      async resolve(parent, args) {
+        const User = require("../models/users");
+
+        try {
+          const deletedUser = await User.findOneAndDelete({
+            email: args.email,
+          });
+
+          if (!deletedUser) {
+            throw new Error("User not found");
+          }
+
+          return {
+            email: deletedUser.email,
+            message: `User deleted with email: ${deletedUser.email}`,
+          };
+        } catch (error) {
+          throw new Error("Error deleting user: " + error.message);
+        }
+      },
+    },
+  },
+});
+
 module.exports = new GraphQLSchema({
   query: RootQuery,
+  mutation: Mutation,
 });
--- models/users.js
+++ models/users.js
@@ -4,7 +4,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   username: String,
   password: String,
-  // Other fields you might have
 });
 
 module.exports = mongoose.model("User", userSchema, "makeup users");
