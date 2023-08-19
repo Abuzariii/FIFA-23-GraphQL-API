c3f90b9bca8a19cd199c683fb2503223dbd3a275
Muhammad Abuar <muhammadabuzar010@gmail.com>
2023-08-19 13:57:29 +0500

    Added more queries

--- graphql/queries.js
+++ graphql/queries.js
@@ -17,4 +17,115 @@ const TopPlayers = async (_, args) => {
   }
 };
 
-module.exports = { TopPlayers };
+const TopPlayersByClub = async (_, args) => {
+  // Use _ as the first argument if it's not being used
+  try {
+    const query = `SELECT * FROM fifa WHERE "Club" = '${args.club}' ORDER BY "Overall" DESC;`;
+    const result = await pool.query(query);
+
+    if (args.limit !== undefined && args.limit > 0) {
+      return result.rows.slice(0, args.limit); // Return the specified number of players
+    }
+
+    return result.rows; // Return all players if no limit is provided
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+};
+
+const TopPlayersByNationality = async (_, args) => {
+  // Use _ as the first argument if it's not being used
+  try {
+    const query = `SELECT * FROM fifa WHERE "Nationality" = '${args.nationality}' ORDER BY "Overall" DESC;`;
+    const result = await pool.query(query);
+
+    if (args.limit !== undefined && args.limit > 0) {
+      return result.rows.slice(0, args.limit); // Return the specified number of players
+    }
+
+    return result.rows; // Return all players if no limit is provided
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+};
+
+const PlayerByName = async (_, args) => {
+  // Use _ as the first argument if it's not being used
+  try {
+    const query = `SELECT * FROM fifa WHERE unaccent("Name") ILIKE unaccent('%${args.name}%') ORDER BY "Overall" DESC;`;
+    const result = await pool.query(query);
+
+    if (args.limit !== undefined && args.limit > 0) {
+      return result.rows.slice(0, args.limit); // Return the specified number of players
+    }
+
+    return result.rows; // Return all players if no limit is provided
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+};
+
+const PlayerByPreferredFoot = async (_, args) => {
+  // Use _ as the first argument if it's not being used
+  try {
+    const query = `SELECT * FROM fifa WHERE initcap("Preferred_Foot") ILIKE initcap('${args.foot}') ORDER BY "Overall" DESC;
+`;
+    const result = await pool.query(query);
+
+    if (args.limit !== undefined && args.limit > 0) {
+      return result.rows.slice(0, args.limit); // Return the specified number of players
+    }
+
+    return result.rows; // Return all players if no limit is provided
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+};
+
+const PlayersByInternationalReputation = async (_, args) => {
+  // Use _ as the first argument if it's not being used
+  try {
+    const query = `SELECT * FROM fifa ORDER BY "International_Reputation" DESC;`;
+    const result = await pool.query(query);
+
+    if (args.limit !== undefined && args.limit > 0) {
+      return result.rows.slice(0, args.limit);
+    }
+
+    return result.rows;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+};
+
+const PlayersByWeakFootPerformance = async (_, args) => {
+  // Use _ as the first argument if it's not being used
+  try {
+    const query = `SELECT * FROM fifa ORDER BY "Weak_Foot" DESC;`;
+    const result = await pool.query(query);
+
+    if (args.limit !== undefined && args.limit > 0) {
+      return result.rows.slice(0, args.limit);
+    }
+
+    return result.rows;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+};
+
+module.exports = {
+  TopPlayers,
+  TopPlayersByClub,
+  TopPlayersByNationality,
+  PlayerByName,
+  PlayerByPreferredFoot,
+  PlayersByInternationalReputation,
+  PlayersByWeakFootPerformance,
+};
--- graphql/schema.js
+++ graphql/schema.js
@@ -3,8 +3,17 @@ const {
   GraphQLObjectType,
   GraphQLList,
   GraphQLInt,
+  GraphQLString,
 } = require("graphql");
-const { TopPlayers } = require("./queries");
+const {
+  TopPlayers,
+  TopPlayersByClub,
+  TopPlayersByNationality,
+  PlayerByName,
+  PlayerByPreferredFoot,
+  PlayersByInternationalReputation,
+  PlayersByWeakFootPerformance,
+} = require("./queries");
 const { PlayerType } = require("./types");
 
 const RootQuery = new GraphQLObjectType({
@@ -16,6 +25,48 @@ const RootQuery = new GraphQLObjectType({
         limit: { type: GraphQLInt },
       },
       resolve: TopPlayers,
+    },
+    Top_Players_By_Club: {
+      type: new GraphQLList(PlayerType),
+      args: {
+        club: { type: GraphQLString },
+      },
+      resolve: TopPlayersByClub,
+    },
+    Top_Players_By_Nationality: {
+      type: new GraphQLList(PlayerType),
+      args: {
+        nationality: { type: GraphQLString },
+      },
+      resolve: TopPlayersByNationality,
+    },
+    Player_By_Name: {
+      type: new GraphQLList(PlayerType),
+      args: {
+        name: { type: GraphQLString },
+      },
+      resolve: PlayerByName,
+    },
+    Player_By_Preferred_Foot: {
+      type: new GraphQLList(PlayerType),
+      args: {
+        foot: { type: GraphQLString },
+      },
+      resolve: PlayerByPreferredFoot,
+    },
+    Player_By_International_Reputation: {
+      type: new GraphQLList(PlayerType),
+      args: {
+        limit: { type: GraphQLInt },
+      },
+      resolve: PlayersByInternationalReputation,
+    },
+    Player_By_Weak_Foot_Ability: {
+      type: new GraphQLList(PlayerType),
+      args: {
+        limit: { type: GraphQLInt },
+      },
+      resolve: PlayersByWeakFootPerformance,
     },
   },
 });
